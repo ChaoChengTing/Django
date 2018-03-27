737bb62a77b2d4d969bd6e84fbec7a901b371a8a
a104590036 <t104590036@ntut.org.tw>
2018-03-27 19:47:25 +0800

    test image

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+import os
 # Create your models here.
+#def get_image_path(instance, filename):
+#    return os.path.join('posts', str(instance.id), filename)
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    #profile_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
 
     def __unicode__(self):
         return self.title
